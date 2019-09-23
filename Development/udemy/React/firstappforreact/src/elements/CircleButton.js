cad9c200db18b98e59c01c62666e19f700a77090
片岡亮 <kataokaryo@kataokaryounoMacBook-puro.local>
2019-09-23 14:56:02 +0900

    Referctering MemoList

---
+++ Development/udemy/React/firstappforreact/src/elements/CircleButton.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class CircleButton extends React.Component {
+  render() {
+    return (
+      <View style={styles.circleButton}>
+        <Text style={styles.circleBottonTitle}>
+          {this.props.children}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  circleButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 40,
+    height: 40,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 2,
+  },
+  circleBottonTitle: {
+    fontSize: 24,
+    lineHeight: 24,
+    color: '#FFF',
+  },
+});
+
+export default CircleButton;
